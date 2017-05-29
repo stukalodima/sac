d403e5edb74106c202ae79802aac26c2c96896bc
dima.stukalo <dima.stukalo@gmail.com>
2017-05-29 18:57:27 +0300

    update 2

--- SSHConnector.py
+++ SSHConnector.py
@@ -4,9 +4,25 @@ from sshtunnel import SSHTunnelForwarder
 
 # noinspection PyBroadException
 class SSHConnector:
+    use_ssh = False
     tableResult = []
+    adm_level1 = dict()
+    adm_level2 = dict()
+    settlement = dict()
+    streets = dict()
+    chains_adm_level = dict()
 
-    def __init__(self, text_sql):
+    def __init__(self, use_ssh):
+        self.use_ssh = use_ssh
+
+    def get_adm_level1_word(self, use_ssh):
+        self.use_ssh = use_ssh
+        text = (" SELECT w.word AS word, w.id AS wordID, mp.id AS mapID, st_astext(mp.centroid) AS centroid, n.name\n"
+                "FROM adm_level1 a\n"
+                "  INNER JOIN links l ON a.id = l.object_id\n"
+                "  INNER JOIN map_objects mp ON mp.id = l.object_id AND mp.category_n = 5\n"
+                "  INNER JOIN names n ON n.name_id = mp.name_id AND n.lang = 'ru'\n"
+                "  INNER JOIN words w ON w.id = l.word_id")
         try:
             with SSHTunnelForwarder(
                     ('s2.sngtrans.com.ua', 22),
@@ -14,7 +30,6 @@ class SSHConnector:
                     ssh_password="D!m@$tuk@l0",
                     remote_bind_address=('sac', 5432)) as server:
                 server.start()
-                # print("server connected")
 
                 params = {
                     'database': 'sac_db',
@@ -26,14 +41,181 @@ class SSHConnector:
 
                 connection = psycopg2.connect(**params)
                 cursor = connection.cursor()
-                # print("database connected")
 
-                cursor.execute(text_sql)
-                self.tableResult = cursor.fetchall()
+                cursor.execute(text)
+                row = cursor.fetchone()
+                while row is not None:
+                    if row[0] in self.adm_level1.keys():
+                        el = self.adm_level1.get(row[0])
+                        el.append(row)
+                        self.adm_level1.update({row[0]: el})
+                    else:
+                        self.adm_level1.update({row[0]: [row]})
+                    row = cursor.fetchone()
 
                 connection.close()
         except:
-            print("Connection Failed: " + text_sql)
+            print("Connection Failed: " + text)
+
+    def get_adm_level2_word(self, use_ssh):
+        self.use_ssh = use_ssh
+        text = ("SELECT w.word AS word, w.id AS wordID, mp.id AS mapID, st_astext(mp.centroid) AS centroid, n.name\n"
+                "FROM adm_level2 a\n"
+                "  INNER JOIN links l ON a.id = l.object_id\n"
+                "  INNER JOIN map_objects mp ON mp.id = l.object_id AND mp.category_n = 4\n"
+                "  INNER JOIN names n ON n.name_id = mp.name_id AND n.lang = 'ru'\n"
+                "  INNER JOIN words w ON w.id = l.word_id")
+        try:
+            with SSHTunnelForwarder(
+                    ('s2.sngtrans.com.ua', 22),
+                    ssh_username="uit.dima",
+                    ssh_password="D!m@$tuk@l0",
+                    remote_bind_address=('sac', 5432)) as server:
+                server.start()
+
+                params = {
+                    'database': 'sac_db',
+                    'user': 'postgres',
+                    'password': 'postgres',
+                    'host': '127.0.0.1',
+                    'port': server.local_bind_port
+                }
+
+                connection = psycopg2.connect(**params)
+                cursor = connection.cursor()
+
+                cursor.execute(text)
+                row = cursor.fetchone()
+                while row is not None:
+                    if row[0] in self.adm_level2.keys():
+                        el = self.adm_level2.get(row[0])
+                        el.append(row)
+                        self.adm_level2.update({row[0]: el})
+                    else:
+                        self.adm_level2.update({row[0]: [row]})
+                    row = cursor.fetchone()
+
+                connection.close()
+        except:
+            print("Connection Failed: " + text)
+
+    def get_settlement_word(self, use_ssh):
+        self.use_ssh = use_ssh
+        text = (" SELECT w.word AS word, w.id AS wordID, mp.id AS mapID, st_astext(mp.centroid) AS centroid, n.name,a.id\n"
+                "FROM settlements a\n"
+                "  INNER JOIN links l ON a.id = l.object_id\n"
+                "  INNER JOIN map_objects mp ON mp.id = l.object_id AND mp.category_n = 3\n"
+                "  INNER JOIN names n ON n.name_id = mp.name_id AND n.lang = 'ru'\n"
+                "  INNER JOIN words w ON w.id = l.word_id")
+        try:
+            with SSHTunnelForwarder(
+                    ('s2.sngtrans.com.ua', 22),
+                    ssh_username="uit.dima",
+                    ssh_password="D!m@$tuk@l0",
+                    remote_bind_address=('sac', 5432)) as server:
+                server.start()
+
+                params = {
+                    'database': 'sac_db',
+                    'user': 'postgres',
+                    'password': 'postgres',
+                    'host': '127.0.0.1',
+                    'port': server.local_bind_port
+                }
+
+                connection = psycopg2.connect(**params)
+                cursor = connection.cursor()
+
+                cursor.execute(text)
+                row = cursor.fetchone()
+                while row is not None:
+                    if row[0] in self.settlement.keys():
+                        el = self.settlement.get(row[0])
+                        el.append(row)
+                        self.settlement.update({row[0]: el})
+                    else:
+                        self.settlement.update({row[0]: [row]})
+                    row = cursor.fetchone()
+
+                connection.close()
+        except:
+            print("Connection Failed: " + text)
+
+    def get_streets_word(self, use_ssh):
+        self.use_ssh = use_ssh
+        text = ("SELECT w.word AS word, w.id AS wordID, mp.id AS mapID, st_astext(mp.centroid) AS centroid, n.name AS name  \n"
+                "FROM map_objects mp\n"
+                "INNER JOIN links l ON mp.id = l.object_id\n"
+                "INNER JOIN words w ON w.id = l.word_id\n"
+                "INNER JOIN names n ON n.name_id = mp.name_id AND n.lang = 'ru'\n"
+                "WHERE mp.category_n = 2")
+        try:
+            with SSHTunnelForwarder(
+                    ('s2.sngtrans.com.ua', 22),
+                    ssh_username="uit.dima",
+                    ssh_password="D!m@$tuk@l0",
+                    remote_bind_address=('sac', 5432)) as server:
+                server.start()
+
+                params = {
+                    'database': 'sac_db',
+                    'user': 'postgres',
+                    'password': 'postgres',
+                    'host': '127.0.0.1',
+                    'port': server.local_bind_port
+                }
+
+                connection = psycopg2.connect(**params)
+                cursor = connection.cursor()
+
+                cursor.execute(text)
+                row = cursor.fetchone()
+                while row is not None:
+                    if row[0] in self.streets.keys():
+                        el = self.streets.get(row[0])
+                        el.append(row)
+                        self.streets.update({row[0]: el})
+                    else:
+                        self.streets.update({row[0]: [row]})
+                    row = cursor.fetchone()
+
+                connection.close()
+        except:
+            print("Connection Failed: " + text)
+
+    def get_chains_adm_level(self, use_ssh):
+        self.use_ssh = use_ssh
+        text = ("SELECT s.id, s.level1_id, s.level2_id, s.class_n\n"
+                "FROM settlements s\n")
+        try:
+            with SSHTunnelForwarder(
+                    ('s2.sngtrans.com.ua', 22),
+                    ssh_username="uit.dima",
+                    ssh_password="D!m@$tuk@l0",
+                    remote_bind_address=('sac', 5432)) as server:
+                server.start()
+
+                params = {
+                    'database': 'sac_db',
+                    'user': 'postgres',
+                    'password': 'postgres',
+                    'host': '127.0.0.1',
+                    'port': server.local_bind_port
+                }
+
+                connection = psycopg2.connect(**params)
+                cursor = connection.cursor()
+
+                cursor.execute(text)
+                row = cursor.fetchone()
+                while row is not None:
+                    self.chains_adm_level.update({row[0]: row})
+                    row = cursor.fetchone()
+
+                connection.close()
+        except:
+            print("Connection Failed: " + text)
 
     def __del__(self):
         self.tableResult = None
+        self.adm_level1 = None
--- main.py
+++ main.py
@@ -4,28 +4,9 @@ from SSHConnector import SSHConnector as Connector
 
 separators = (",", ".", "-", "(", ")", "/", "\\")
 key_word = {"building", "street", "apt", "str", "app", "ap", "yl", "ul", "fl", "kv", "highway", "bul", "apartment",
-            "avenue", "prospekt", "obl", "ave", "room", "region", "district", "pgt"}
+            "avenue", "prospekt", "obl", "ave", "room", "region", "district", "pgt", "область", "rayon", "selo"}
 
-query_city_id = ("SELECT mp.id AS city\n"
-                 "FROM words AS w\n"
-                 "  INNER JOIN links l ON l.word_id = w.id\n"
-                 "  INNER JOIN map_objects mp ON mp.id = l.object_id\n"
-                 "WHERE w.id IN ({0})\n"
-                 "      AND mp.category_n = 3")
-
-query_street_id = ("SELECT mp.id AS street_id\n"
-                   "FROM words AS w\n"
-                   "  INNER JOIN links l ON l.word_id = w.id\n"
-                   "  INNER JOIN map_objects mp ON mp.id = l.object_id\n"
-                   "WHERE w.id IN ({0})\n"
-                   "      AND mp.category_n = 2")
-
-query_address_id = ("SELECT a.id AS address_id, mp.id AS map_id, ST_AsText(mp.centroid) AS point, n.name\n"
-                    "FROM address a\n"
-                    "  INNER JOIN map_objects mp ON mp.id = a.id\n"
-                    "  INNER JOIN names n ON n.name_id = mp.name_id AND n.lang = 'ru'\n"
-                    "WHERE a.settlement_id IN ({0})\n"
-                    "     AND a.street_id IN ({1})")
+words_table = []
 
 
 def get_array_words(string, this_separator):
@@ -33,15 +14,16 @@ def get_array_words(string, this_separator):
     result = []
     for word in words_array:
         is_with_out_separator = True
+        check_word = word.strip()
         for sep in separators:
-            if word.find(sep) != -1:
+            if check_word.find(sep) != -1:
                 is_with_out_separator = False
-                tpm_result = get_array_words(word, sep)
+                tpm_result = get_array_words(check_word, sep)
                 for tmp_word in tpm_result:
-                    if tmp_word.strip() != "" and tmp_word.strip() not in result:
-                        result.append(tmp_word.strip())
-        if is_with_out_separator and word.lower() not in result:
-            result.append(word.lower())
+                    if tmp_word != "" and tmp_word not in result and tmp_word not in key_word:
+                        result.append(tmp_word)
+        if is_with_out_separator and check_word not in result and check_word not in key_word:
+            result.append(check_word)
     return result
 
 
@@ -82,42 +64,6 @@ def get_word_id_string(string, find_alpha, find_number, comment):
     return word_id_array
 
 
-def get_word_id_aray(string, find_alpha, find_number, comment):
-    words_array = get_array_words(string, " ")
-    word_id_array = []
-    print("!!!" + comment + "!!!")
-    for word in words_array:
-        find = False
-        if word in key_word or (len(word) == 1 and word.isalpha()) or len(word) == 0:
-            continue
-        if word.isalpha() and find_number:
-            continue
-        if word.isdigit() and find_alpha:
-            continue
-        count_a = 0
-        count_n = 0
-        for alp in word:
-            if alp.isalpha():
-                count_a += 1
-            if alp.isdigit():
-                count_n += 1
-        if count_n > 0 and (count_a == 1 or count_a == 2):
-            continue
-        for word_row in words_table:
-            if word == word_row[1]:
-                print(word_row[1] + " true find")
-                find = True
-                word_id_array.append(word_row[0])
-            if find:
-                break
-        if not find:
-            for word_row in words_table:
-                if distance(word, word_row[1]) <= (int(len(word) / 4) + 1):
-                    print(word_row[1])
-                    word_id_array.append(word_row[0])
-    return word_id_array
-
-
 def distance(a, b):
     n, m = len(a), len(b)
     if n > m:
@@ -136,153 +82,105 @@ def distance(a, b):
     return current_row[n]
 
 
-def get_wordID_linkID_array(array_wordID_linkID, array_wordID):
-    for wordID in array_wordID:
-        for linkID_row in links_table:
-            if wordID == linkID_row[0]:
-                array_wordID_linkID.append()
+def get_word_array(first_string, second_string):
+    print("Получаем слова по первой колонке...")
+    result_array = get_array_words(first_string.lower(), " ")
+    if len(result_array) == 0:
+        print("В первой колонке слов нет!!!")
+        print("Получаем слова по второй колонке...")
+        result_array = get_array_words(second_string.lower())
+        print(second_string)
+    else:
+        print(first_string)
+    return result_array
 
 
+def get_adm_level(word_array, word_array_from_db, comment):
+    result_array = dict()
+    for word in word_array:
+        true_search = word in word_array_from_db.keys()
+        if true_search:
+            row_list = word_array_from_db.get(word)
+            for row in row_list:
+                result_array.update({row[2]: (row[3], row[4])})
+    if len(result_array) == 0:
+        for word in word_array:
+            mistake_count = int(len(word) / 4)
+            if mistake_count == 0:
+                continue
+            for key in word_array_from_db.keys():
+                if distance(key, word) <= mistake_count:
+                    row_list = word_array_from_db.get(key)
+                    for row in row_list:
+                        result_array.update({row[2]: (row[3], row[4])})
+    if len(result_array) != 0:
+        print("Получили {0}:".format(comment))
+        for key, value in result_array.items():
+            print(str(key) + " | " + str(value))
+    else:
+        print("{0} не определили".format(comment))
+    print("\n")
+    return result_array
+
+
+print("Читаем файл адресов")
 reader = ExcelReader('inputFile.xlsx')
 table = reader.table
 address_table = []
 for line in table:
     address = AddressLine(*line)
     address_table.append(address)
+print("Закончили читать файл адресов")
 
-databaseConnector = Connector("SELECT * FROM words")
-words_table = databaseConnector.tableResult
-del databaseConnector
-databaseConnector = Connector("SELECT * \n"
-                              "FROM links l")
-links_table = databaseConnector.tableResult
-del databaseConnector
-exit(0)
+data_base = Connector(True)
+# print("Читаем таблицу областей...")
+# data_base.get_adm_level1_word(True)
+# print("Закончили читать таблицу областей...")
+# print("Читаем таблицу районов...")
+# data_base.get_adm_level2_word(True)
+# print("Закончили читать таблицу районов...")
+print("Читаем таблицу городов...")
+data_base.get_settlement_word(True)
+print("Закончили читать таблицу городов...")
+# print("Читаем таблицу улиц...")
+# data_base.get_streets_word(True)
+# print("Закончили читать таблицу улиц...")
+# print("Читаем таблицу привязок адм. единиц...")
+# data_base.get_chains_adm_level(True)
+# print("Закончили читать таблицу привязок адм. единиц...")
+
 row_count = 0
-start_row = 34
-end_row = 34
+start_row = 2390
+end_row = 2410
+print("Побежали по строкам")
+print("\n")
 for line in address_table:
     row_count += 1
     if row_count < start_row:
         continue
     if row_count > end_row:
         break
-    print(line)
-
-    # /////////////////////////////////
-    # array_wordID = get_word_id_aray()
-    # array_wordID_linkID = []
-    # get_wordID_linkID_array(array_wordID_linkID, array_wordID)
-    # /////////////////////////////////
-
-    # f = open('/home/dima/result.txt', 'a')
-    # f.write(line.city + " " + line.address)
-    # do with city
-    word_id_array = get_word_id_string(line.city, True, False, "Search city")
-    print("city word id:" + word_id_array)
-    is_address_parse = False
-    if len(word_id_array) == 0:
-        word_id_array = get_word_id_string(line.address, True, False, "Search city by address column")
-        print("city word id:" + word_id_array)
-        is_address_parse = True
-    if word_id_array == "":
-        print("CITY NOT FIND")
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    print("Обрабатываем строку №" + str(row_count))
+    if len(line.city) == 0 and len(line.address) == 0:
+        print("Обе колонки адреса пусты... искать нечего")
         continue
-    city_obj_table = Connector(query_city_id.format(word_id_array)).tableResult
-    city_obj_id = ""
-    for el in city_obj_table:
-        city_obj_id += (str(el[0]) + ",")
-    city_obj_id = city_obj_id[:len(city_obj_id) - 1]
-    print("city object ID:" + city_obj_id)
-
-    # do with street
-    if not is_address_parse:
-        word_id_array = get_word_id_string(line.address, True, False, "Search street")
-    else:
-        print("Street was parse with city")
-    print("street word id:" + word_id_array)
-    if word_id_array == "":
-        print("STREET NOT FIND")
-        continue
-    street_obj_table = Connector(query_street_id.format(word_id_array)).tableResult
-    street_obj_id = ""
-    for el in street_obj_table:
-        street_obj_id += (str(el[0]) + ",")
-    street_obj_id = street_obj_id[:len(street_obj_id) - 1]
-    print("street object ID:" + street_obj_id)
-
-    # do with apartment
-    # word_id_array = get_word_id_string(line.address, False, True, "Search apartment")
-    # print("apartment word id:" + word_id_array)
-    # app_obj_table = Connector(query_app_id.format(word_id_array)).tableResult
-    # app_obj_id = set()
-    # for el in app_obj_table:
-    #     app_obj_id.add(el[0])
-    # print(app_obj_id)
-
-    address_obj_table = Connector(query_address_id.format(city_obj_id, street_obj_id)).tableResult
-    address_obj_id = set()
-    apartment = get_array_words(line.address, " ")
-    apartment_key = ""
-    for ap in apartment:
-        count_a = 0
-        count_n = 0
-        for alp in ap:
-            if alp.isalpha():
-                count_a += 1
-            if alp.isdigit():
-                count_n += 1
-        if count_n > 0 and (count_a == 1 or count_a == 2):
-            for alp in ap:
-                if alp.isdigit():
-                    apartment_key += alp
-                if alp.isalpha():
-                    apartment_key += (alp.lower())
-            break
-        if ap.isdigit():
-            apartment_key = ap
-            break
-    if apartment_key == "":
-        print("APARTMENT NOT IN LINE")
-        continue
-    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    # f.write(" | ")
-    find_apartment = False
-    print("apartment " + apartment_key)
-    for el in address_obj_table:
-        print("address:")
-        print(el)
-        if el[3] == apartment_key:
-            print(el[2])
-            find_apartment = True
-            break
-        else:
-            print("Not find")
-    if not find_apartment:
-        for el in address_obj_table:
-            if distance(el[3], apartment_key) <= 1:
-                print(el[2])
-                # f.write(el[2])
-    # f.write("\n")
-    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    # f.close()
-    # address_obj_id.add(el[0])
-    # print("Address ID:" + str(address_obj_id))
-    # res_set = address_obj_id.intersection(app_obj_id)
-    # print("Intersection address and apartment:" + str(res_set))
-    #
-    # # do final query
-    # address_obj_id_str = ""
-    # for el_address in res_set:
-    #     address_obj_id_str += (str(el[0]) + ",")
-    #     address_obj_id_str = address_obj_id_str[:len(address_obj_id_str) - 1]
-    #
-    # line_result = Connector(query_final_data.format(address_obj_id_str)).tableResult
-    # if len(line_result) == 0:
-    #     print("Not found")
-    # res_count = 0
-    # for el in line_result:
-    #     print("RESULT " + str(res_count) + ":" + el[1])
-
-    # if row_count == 20:
-    #     break
+    print("Формируем слова для поиска адм. одиниц\n")
+    words_array_city = get_word_array(line.city, line.address)
+    # print("Формируем слова для поиска улиц\n")
+    # words_array_street = get_word_array(line.address, line.city)
+    # print("Пробуем определить область")
+    # adm_level1 = get_adm_level(words_array_city, data_base.adm_level1, "Области")
+    # print("Пробуем определить районы")
+    # adm_level2 = get_adm_level(words_array_city, data_base.adm_level2, "Районы")
+    print("Пробуем определить города")
+    settlement = get_adm_level(words_array_city, data_base.settlement, "Города")
+    # print("Пробуем определить улицу")
+    # street = get_adm_level(words_array_street, data_base.streets, "Улицы")
+    # print("Ищем пересечения по адм. единицам...")
+    # for city in settlement.keys():
+    #     summ_find = 0;
+    #     row_chains = data_base.chains_adm_level.get(city)
+    #     if row_chains is not None:
+    #         if row_chains[]
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
