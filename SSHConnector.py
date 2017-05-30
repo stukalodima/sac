9c5dc3a58acff438b135e89b824ba3647cecbf22
dima.stukalo <dima.stukalo@gmail.com>
2017-05-30 19:20:28 +0300

    update 2

--- SSHConnector.py
+++ SSHConnector.py
@@ -17,7 +17,8 @@ class SSHConnector:
 
     def get_adm_level1_word(self, use_ssh):
         self.use_ssh = use_ssh
-        text = (" SELECT w.word AS word, w.id AS wordID, mp.id AS mapID, st_astext(mp.centroid) AS centroid, n.name\n"
+        text = (" SELECT w.word AS word, w.id AS wordID, mp.id AS mapID, st_astext(mp.centroid) AS centroid, n.name,\n"
+                "a.adm_center_id\n"
                 "FROM adm_level1 a\n"
                 "  INNER JOIN links l ON a.id = l.object_id\n"
                 "  INNER JOIN map_objects mp ON mp.id = l.object_id AND mp.category_n = 5\n"
@@ -59,7 +60,8 @@ class SSHConnector:
 
     def get_adm_level2_word(self, use_ssh):
         self.use_ssh = use_ssh
-        text = ("SELECT w.word AS word, w.id AS wordID, mp.id AS mapID, st_astext(mp.centroid) AS centroid, n.name\n"
+        text = ("SELECT w.word AS word, w.id AS wordID, mp.id AS mapID, st_astext(mp.centroid) AS centroid, n.name,\n"
+                "a.level1_id\n"
                 "FROM adm_level2 a\n"
                 "  INNER JOIN links l ON a.id = l.object_id\n"
                 "  INNER JOIN map_objects mp ON mp.id = l.object_id AND mp.category_n = 4\n"
@@ -101,7 +103,8 @@ class SSHConnector:
 
     def get_settlement_word(self, use_ssh):
         self.use_ssh = use_ssh
-        text = (" SELECT w.word AS word, w.id AS wordID, mp.id AS mapID, st_astext(mp.centroid) AS centroid, n.name,a.id\n"
+        text = (" SELECT w.word AS word, w.id AS wordID, mp.id AS mapID, st_astext(mp.centroid) AS centroid, n.name,\n"
+                "a.level2_id, a.level1_id, a.class_n\n"
                 "FROM settlements a\n"
                 "  INNER JOIN links l ON a.id = l.object_id\n"
                 "  INNER JOIN map_objects mp ON mp.id = l.object_id AND mp.category_n = 3\n"
@@ -143,12 +146,12 @@ class SSHConnector:
 
     def get_streets_word(self, use_ssh):
         self.use_ssh = use_ssh
-        text = ("SELECT w.word AS word, w.id AS wordID, mp.id AS mapID, st_astext(mp.centroid) AS centroid, n.name AS name  \n"
-                "FROM map_objects mp\n"
-                "INNER JOIN links l ON mp.id = l.object_id\n"
-                "INNER JOIN words w ON w.id = l.word_id\n"
-                "INNER JOIN names n ON n.name_id = mp.name_id AND n.lang = 'ru'\n"
-                "WHERE mp.category_n = 2")
+        text = ("SELECT w.word word, w.id wordID, mp.id mapID, mp.centroid centroid, n.name, s.settlement_id\n"
+                "FROM streets AS s\n"
+                "  INNER JOIN links l ON s.id = l.object_id\n"
+                "  INNER JOIN map_objects mp ON mp.id = l.object_id AND mp.category_n = 2\n"
+                "  INNER JOIN names n ON n.name_id = mp.name_id AND n.lang = 'ru'\n"
+                "  INNER JOIN words w ON w.id = l.word_id")
         try:
             with SSHTunnelForwarder(
                     ('s2.sngtrans.com.ua', 22),
@@ -212,6 +215,61 @@ class SSHConnector:
                     self.chains_adm_level.update({row[0]: row})
                     row = cursor.fetchone()
 
+                connection.close()
+        except:
+            print("Connection Failed: " + text)
+
+    def select_from_db(self, text, use_ssh):
+        self.use_ssh = use_ssh
+        try:
+            with SSHTunnelForwarder(
+                    ('s2.sngtrans.com.ua', 22),
+                    ssh_username="uit.dima",
+                    ssh_password="D!m@$tuk@l0",
+                    remote_bind_address=('sac', 5432)) as server:
+                server.start()
+
+                params = {
+                    'database': 'sac_db',
+                    'user': 'postgres',
+                    'password': 'postgres',
+                    'host': '127.0.0.1',
+                    'port': server.local_bind_port
+                }
+
+                connection = psycopg2.connect(**params)
+                cursor = connection.cursor()
+
+                cursor.execute(text)
+                self.tableResult = cursor.fetchall()
+
+                connection.close()
+        except:
+            print("Connection Failed: " + text)
+
+    def insert_into_db(self, text, use_ssh):
+        self.use_ssh = use_ssh
+        try:
+            with SSHTunnelForwarder(
+                    ('s2.sngtrans.com.ua', 22),
+                    ssh_username="uit.dima",
+                    ssh_password="D!m@$tuk@l0",
+                    remote_bind_address=('sac', 5432)) as server:
+                server.start()
+
+                params = {
+                    'database': 'sac_db',
+                    'user': 'postgres',
+                    'password': 'postgres',
+                    'host': '127.0.0.1',
+                    'port': server.local_bind_port
+                }
+
+                connection = psycopg2.connect(**params)
+                cursor = connection.cursor()
+
+                cursor.execute(text)
+                connection.commit()
                 connection.close()
         except:
             print("Connection Failed: " + text)
---
+++ indexLoad.py
@@ -0,0 +1,148 @@
+from ExcelReader import ExcelReader
+from SSHConnector import SSHConnector as Connector
+
+separators = (",", ".", "-", "(", ")", "/", "\\")
+key_word = dict()
+
+# obl = dict({1: ""})
+# rn = dict({"2": ""})
+# city = dict({"3": ""})
+# street = dict({"4": ""})
+# index_id = 3
+#
+# data_base = Connector(True)
+# parameters = {"adm_level1": int("".join(obl.keys())), "adm_level2": int("".join(rn.keys())),
+#               "settlement": int("".join(city.keys())),"street": int("".join(street.keys())), "index": int(index_id)}
+# data_base.insert_into_db("INSERT INTO index_chains (adm_level1, adm_level2, settlement, street, index) \n"
+#                          "VALUES (%(adm_level1)d, %(adm_level2)d, %(settlement)d, %(street)d, %(index)d)" % parameters,
+#                          True)
+
+# exit(0)
+
+
+def get_array_words(string, this_separator):
+    words_array = string.split(this_separator)
+    result = []
+    for word in words_array:
+        is_with_out_separator = True
+        check_word = word.strip()
+        for sep in separators:
+            if check_word.find(sep) != -1:
+                is_with_out_separator = False
+                tpm_result = get_array_words(check_word, sep)
+                for tmp_word in tpm_result:
+                    if tmp_word != "" and tmp_word not in result and tmp_word not in key_word:
+                        result.append(tmp_word)
+        if is_with_out_separator and check_word not in result and check_word not in key_word:
+            result.append(check_word)
+    return result
+
+
+def get_word_array(first_string, second_string):
+    print("Получаем слова по первой колонке...")
+    result_array = get_array_words(first_string.lower(), " ")
+    if len(result_array) == 0:
+        print("В первой колонке слов нет!!!")
+        print("Получаем слова по второй колонке...")
+        result_array = get_array_words(second_string.lower(), " ")
+        print(second_string)
+    else:
+        print(first_string)
+    return result_array
+
+
+def filter_dict(filtered_dict, owner, comment):
+    if len(filtered_dict) > 1:
+        list_keys_for_delete = []
+        for key, value in filtered_dict.items():
+            if value[2] not in owner.keys():
+                list_keys_for_delete.append(key)
+        if len(list_keys_for_delete) > 0:
+            for key in list_keys_for_delete:
+                filtered_dict.pop(key)
+    print(comment + str(filtered_dict))
+    return filtered_dict
+
+
+def get_adm_level(word_array, word_array_from_db, comment):
+    result_array = dict()
+    for word in word_array:
+        true_search = word in word_array_from_db.keys()
+        if true_search:
+            row_list = word_array_from_db.get(word)
+            for row in row_list:
+                result_array.update({row[2]: (row[3], row[4], row[5])})
+    if len(result_array) != 0:
+        print("Получили {0}:".format(comment))
+        for key, value in result_array.items():
+            print(str(key) + " | " + str(value))
+    else:
+        print("{0} не определили".format(comment))
+    print("\n")
+    return result_array
+
+
+def get_key_from_dict(dict_for_search):
+    is_key = 0
+    for key in dict_for_search.keys():
+        is_key = key
+    return is_key
+
+
+index_file = ExcelReader("indexes1.xlsx")
+index_table = index_file.table
+
+data_base = Connector(True)
+print("Читаем таблицу областей...")
+data_base.get_adm_level1_word(True)
+print("Закончили читать таблицу областей...")
+print("Читаем таблицу районов...")
+data_base.get_adm_level2_word(True)
+print("Закончили читать таблицу районов...")
+print("Читаем таблицу городов...")
+data_base.get_settlement_word(True)
+print("Закончили читать таблицу городов...")
+print("Читаем таблицу улиц...")
+data_base.get_streets_word(True)
+print("Закончили читать таблицу улиц...")
+
+start_row = 25790
+row_count = 0
+for line in index_table:
+    row_count += 1
+    if row_count < start_row:
+        continue
+    if row_count > start_row + 20:
+        break
+    print(line)
+    print("\n")
+    words_array_obl = get_word_array(line[0], "")
+    obl = get_adm_level(words_array_obl, data_base.adm_level1, "Области")
+    words_array_rn = get_word_array(line[1], "")
+    rn = get_adm_level(words_array_rn, data_base.adm_level2, "районы")
+    words_array_city = get_word_array(line[2], "")
+    city = get_adm_level(words_array_city, data_base.settlement, "Города")
+    words_array_street = get_word_array(line[5], "")
+    street = get_adm_level(words_array_street, data_base.streets, "Улицы")
+    index = str(line[3])
+    while len(index) < 5:
+        index = "0" + index
+    print(index)
+    rn = filter_dict(rn, obl, "r-n: ")
+    city = filter_dict(city, rn, "city: ")
+    street = filter_dict(street, city, "street: ")
+    if len(street) == 0:
+        continue
+
+    data_base.select_from_db("SELECT * FROM indexes WHERE index = '%(index)s'" % {"index": index}, True)
+    if len(data_base.tableResult) == 0:
+        data_base.insert_into_db("INSERT INTO indexes (index) VALUES ('%(index)s')" % {"index": index}, True)
+        data_base.select_from_db("SELECT * FROM indexes WHERE index = '%(index)s'" % {"index": index}, True)
+
+    index_id = data_base.tableResult[0][0]
+
+    parameters = {"adm_level1": get_key_from_dict(obl), "adm_level2": get_key_from_dict(rn),
+                  "settlement": get_key_from_dict(city), "street": get_key_from_dict(street), "index": index_id}
+    data_base.insert_into_db("INSERT INTO index_chains (adm_level1, adm_level2, settlement, street, index) \n"
+                             "VALUES (%(adm_level1)s, %(adm_level2)s, %(settlement)s, %(street)s, %(index)s)\n"
+                             "" % parameters, True)
